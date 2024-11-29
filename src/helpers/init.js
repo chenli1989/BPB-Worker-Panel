400492920ced711bc8be511f0b63b86877252d58
chenls <chenli_1989@foxmail.com>
2024-11-29 01:54:10 +0000

    chore: update 2.7.6.1

--- src/helpers/init.js
+++ src/helpers/init.js
@@ -1,17 +1,17 @@
 import { isValidUUID } from './helpers';
-const defaultProxyIP = 'bpb.yousef.isegaro.com';
+const defaultProxyIP = '192.237.192.175';
 let userID, dohURL, proxyIP, trojanPassword, defaultHttpPorts, defaultHttpsPorts, panelVersion, hostName, origin, client, pathName;
 
 function initParams(request, env) {
     const proxyIPs = env.PROXYIP?.split(',').map(proxyIP => proxyIP.trim());
-    userID = env.UUID || '89b3cbba-e6ac-485a-9481-976a0415eab9';
+    userID = env.UUID || 'e9b267bc-63be-4eb4-a6b6-950da2fc3a61';
     if (!isValidUUID(userID)) throw new Error(`Invalid UUID: ${userID}`);
     dohURL = env.DOH_URL || 'https://cloudflare-dns.com/dns-query';
     proxyIP = proxyIPs ? proxyIPs[Math.floor(Math.random() * proxyIPs.length)] : defaultProxyIP;
-    trojanPassword = env.TROJAN_PASS || 'bpb-trojan';
+    trojanPassword = env.TROJAN_PASS || '950da2fc3a61';
     defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
     defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
-    panelVersion = '2.7.6';
+    panelVersion = '2.7.6.1';
     hostName = request.headers.get('Host');
     const url = new URL(request.url);
     const searchParams = new URLSearchParams(url.search);
